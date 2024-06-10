ab38b6cdd8a5039782222fd73e4dd2c84f4d69ee
Sarath191181208 <vssarathc04@gmail.com>
2024-06-10 22:48:02 +0530

    + the install file to use stow, and + cmds

--- install.sh
+++ install.sh
@@ -13,25 +13,61 @@ mkdirCP(){
 }
 
 packages=(
-  "python3",
   "git",
   "gh",
   "zsh",
   "neovim",
-  "kitty",
   "fzf",
   "ripgrep",
   "mcfly",
-  "i3",
-  "i3status",
+  "unzip",
+  "jq", # JSON filter tool
+  "stow", # Symlink farm manager
+  "ttf-jetbrains-mono-nerd" # font
+
+  "python3",
   "nodejs",
-  "npm"
+  "npm",
+  "gnome-terminal", # imp for docker for some reason
+  "docker",
+  "docker-compose",
+  "lazydocker",
+
+  ##########
+  ### DM ###
+  ##########
+  "sway",
+  "swaybg",
+  "wlroots",
+  "foot",
+  "wofi",
+  "wlroots", 
+  "wl-clipboard",
+  "mako", #  notification daemon for Wayland
 )
 
 for package in "${packages[@]}"
 do
     echo "Installing $package"
     installPackage $package
+done
+
+yay_packages = (
+  ###########
+  ### YAY ###
+  ### PKG ###
+  ###########
+  # " xdg-desktop-portal-wlr " # maybe useful for Screenshot
+  "sway-screenshot", # Screenshot tool for sway
+  "catppuccin-gtk-theme-mocha",
+  "landrop", # local file sharing tool
+  "qt5-wayland", # To ensure landrop working with wayland 
+)
+
+for package in "${packages[@]}"
+do
+    echo "Installing $package"
+    sudo yay -S $package
 done
 
 # Installing zsh plugins 
@@ -47,18 +83,20 @@ do
     git clone https://github.com/zsh-users/$plugin.git $ZSH_CUSTOM/plugins/$plugin
 done
 
-# Putting config files in place
-cp .zshrc ~/.zshrc 
-cp .gitconfig ~/.gitconfig 
+stow --adopt .
 
-# i3 config 
-mkdirCP i3/config ~/.config/i3/config
-mkdirCP i3status/config ~/.config/i3status/config
-
-# kitty config
-mkdirCP kitty/kitty.conf ~/.config/kitty/kitty.conf 
-mkdirCP kitty/current-theme.conf ~/.config/kitty/current-theme.conf
-
-# neovim config
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 
-git clone https://github.com/Sarath191181208/nvim-config ~/.config/nvim/lua/custom --depth 1
+# # Putting config files in place
+# cp .zshrc ~/.zshrc 
+# cp .gitconfig ~/.gitconfig 
+#
+# # i3 config 
+# mkdirCP i3/config ~/.config/i3/config
+# mkdirCP i3status/config ~/.config/i3status/config
+#
+# # kitty config
+# mkdirCP kitty/kitty.conf ~/.config/kitty/kitty.conf 
+# mkdirCP kitty/current-theme.conf ~/.config/kitty/current-theme.conf
+#
+# # neovim config
+# git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 
+# git clone https://github.com/Sarath191181208/nvim-config ~/.config/nvim/lua/custom --depth 1
