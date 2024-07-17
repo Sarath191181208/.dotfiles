9933730faa4b6c273867141943fef7fbb6f6171a
Sarath191181208 <vssarathc04@gmail.com>
2024-07-17 14:01:29 +0530

    .tmux is considered as `sh` filetype

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,3 +6,11 @@ vim.opt.showmode = false
 
 --adding custom snippets
 vim.g.snipmate_snippets_path = vim.fn.stdpath "config" .. "/lua/snippets"
+
+-- setting file type for .tmux files
+vim.cmd([[
+  augroup filetype_tmux
+    autocmd!
+    autocmd BufRead,BufNewFile .tmux set filetype=sh
+  augroup END
+]])
