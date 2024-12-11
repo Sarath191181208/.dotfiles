baf9065c413abed40b3f396dfff8776edffe4443
Sarath191181208 <vssarathc04@gmail.com>
2024-12-11 13:03:49 +0530

    neoscroll end delim changed

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -93,15 +93,15 @@ M.gitsigns = {
 
 local neoscroll = require('neoscroll')
 local neomaps = {
-  ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 80 }) end;
-  ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 80 }) end;
-  ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 200 }) end;
-  ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 200 }) end;
-  ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor=false; duration = 100 }) end;
-  ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor=false; duration = 100 }) end;
-  ["zt"]    = function() neoscroll.zt({ half_win_duration = 150 }) end;
-  ["zz"]    = function() neoscroll.zz({ half_win_duration = 150 }) end;
-  ["zb"]    = function() neoscroll.zb({ half_win_duration = 150 }) end;
+  ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 80 }) end,
+  ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 80 }) end,
+  ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 200 }) end,
+  ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 200 }) end,
+  ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 }) end,
+  ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor = false, duration = 100 }) end,
+  ["zt"]    = function() neoscroll.zt({ half_win_duration = 150 }) end,
+  ["zz"]    = function() neoscroll.zz({ half_win_duration = 150 }) end,
+  ["zb"]    = function() neoscroll.zb({ half_win_duration = 150 }) end,
 }
 local modes = { 'n', 'v', 'x' }
 for key, func in pairs(neomaps) do
