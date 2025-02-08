0d509a9b35dcc9cd5a3a7c49b45952132d8974ca
Sarath191181208 <vssarathc04@gmail.com>
2025-02-08 12:51:11 +0530

    incl some opts

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -7,6 +7,23 @@ vim.opt.showmode = false
 --adding custom snippets
 vim.g.snipmate_snippets_path = vim.fn.stdpath "config" .. "/lua/snippets"
 
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
 -- setting file type for .tmux files
 vim.cmd([[
   augroup filetype_tmux
